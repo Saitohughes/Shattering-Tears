080f90967c9ca2c44cbba05761746dc35ddd014e
Saito Hughes <julijaraupb5.1@gmail.com>
2024-01-27 21:49:27 -0500

    active ragdoll working

--- Assets/Scripts/UManPlayerController.cs
+++ Assets/Scripts/UManPlayerController.cs
@@ -6,11 +6,23 @@ public class UManPlayerController : MonoBehaviour
 {
     [SerializeField]
     float Score;
+    Rigidbody[] _allrigidbodies;
+
+
+    private void Awake()
+    {
+        _allrigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Rigidbody rb in _allrigidbodies)
+        {
+            rb.solverIterations = 8;
+            rb.solverVelocityIterations = 8;
+            rb.maxAngularVelocity = 30;
+        }
     }
 
     // Update is called once per frame
